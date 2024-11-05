f9f62acb97539cd1f2b75a54a4401340c5dd167b
sagathiatejasopenXcell <132910148+sagathiatejasopenXcell@users.noreply.github.com>
2024-11-05 11:27:01 +0530

    react-native upgraded to 0.76.1

---
+++ android/app/.cxx/Debug/4838352c/prefab/x86/prefab/lib/i686-linux-android/cmake/fbjni/fbjniConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET fbjni::fbjni)
+add_library(fbjni::fbjni SHARED IMPORTED)
+set_target_properties(fbjni::fbjni PROPERTIES
+    IMPORTED_LOCATION "/Users/tejas/.gradle/caches/8.10.2/transforms/6dc174b9394f7f2da85e384b4bbf0f4a/transformed/jetified-fbjni-0.6.0/prefab/modules/fbjni/libs/android.x86/libfbjni.so"
+    INTERFACE_INCLUDE_DIRECTORIES "/Users/tejas/.gradle/caches/8.10.2/transforms/6dc174b9394f7f2da85e384b4bbf0f4a/transformed/jetified-fbjni-0.6.0/prefab/modules/fbjni/include"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
