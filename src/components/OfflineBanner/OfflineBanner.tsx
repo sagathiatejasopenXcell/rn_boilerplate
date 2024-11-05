4db7c58dd4ea83b3ab0e691f653232e5d3b9ad57
sagathiatejasopenXcell <132910148+sagathiatejasopenXcell@users.noreply.github.com>
2024-11-05 17:20:50 +0530

    rn_upgrade merged with offline banner added

--- src/components/OfflineBanner/OfflineBanner.tsx
+++ src/components/OfflineBanner/OfflineBanner.tsx
@@ -10,7 +10,7 @@ export const OfflineBanner = () => {
   const { color } = useColor();
   const styles = offlineBannerStyles(color);
 
-  if (!isConnected) return null;
+  if (isConnected) return null;
 
   return (
     <View style={styles.banner}>
